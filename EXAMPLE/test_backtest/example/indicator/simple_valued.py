113bd7bd76e9308c618da61643d467a827ce1f32
lkaiser <minijjlk@gmail.com>
2019-02-15 08:35:48 +0800

    simple strategy

--- EXAMPLE/test_backtest/example/indicator/simple_valued.py
+++ EXAMPLE/test_backtest/example/indicator/simple_valued.py
@@ -1,16 +1,27 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import QUANTAXIS as QA
 from QUANTAXIS.QAFetch import QATusharePro as pro
 import pandas as pd
 import numpy as np
-
+import datetime
+import os
+#pd.set_option('display.float_format', lambda x: '%.3f' % x)
+#pd.set_option('display.max_columns',5, 'display.max_rows', 100)
 class simpleValued:
 
     def __init__(self,start,end):
-        start_2years_bf = str(int(start) - 2)
+        self.start = start
+        self.end = end
+        self.basic_temp_name = 'basic_temp_' + datetime.datetime.now().strftime('%Y-%m-%d') +'.pkl'
+        start_2years_bf = str(int(start[0:4]) - 3)
         self.finacial = pro.QA_fetch_get_finindicator(start=start_2years_bf,end=end)
+        print(self.finacial.head())
         self.asset = pro.QA_fetch_get_assetAliability(start=start_2years_bf, end=end)
-        self.basic = pro.QA_fetch_get_dailyindicator(start=start,end=end)
+        if (os.path.isfile(self.basic_temp_name)):
+            self.basic = pd.read_pickle(self.basic_temp_name)
+        else:
+            basic = pro.QA_fetch_get_dailyindicator(start=start,end=end)
+            self.basic = basic.sort_values(['ts_code','trade_date'], ascending = True)
         self.stock = pro.QA_SU_stock_info()
         self.dailymarket = None
 
@@ -26,64 +37,91 @@ class simpleValued:
             if(data['q_dt_roe'].isnull().all()):
                 roe_half_year = roe_year = np.array([np.nan] * data.shape[0])
             else:
-                roe_year = QA.EMA(data['q_dt_roe'], 4)*4
-                roe_half_year = QA.EMA(data['q_dt_roe'], 2)*4
+                roe_year = QA.EMA(data['q_dt_roe'], 4)*4/100+1
+                roe_half_year = QA.EMA(data['q_dt_roe'], 2)*4/100+1
             #近2年净资产收益率
-            asset_rise_2year = data['equity_yoy'].shift(4)*data['equity_yoy']
+            asset_rise_2year = (data['equity_yoy'].shift(4)/100+1)*(data['equity_yoy']/100+1)
             # 近3年净资产收益率
-            asset_rise_3year = data['equity_yoy'].shift(8) * asset_rise_2year
-            data.loc[:,'equity_pb6'] = np.power(asset_rise_3year,6);
-            data.loc[:, 'equity_pb7'] = np.power(asset_rise_3year, 7);
-            data.loc[:, 'roe_year_pb6'] = np.power(roe_year, 6);
-            data.loc[:, 'roe_year_pb7'] = np.power(roe_year, 7);
+            asset_rise_3year = (data['equity_yoy'].shift(8)/100+1) * asset_rise_2year
+            data.loc[:,'equity3_pb7'] = np.round(np.power(np.power(asset_rise_3year,1/3),7),2)
+            #print(len(asset_rise_3year[asset_rise_3year.isnull()]))
+            data.loc[:, 'equity2_pb7'] = np.round(np.power(np.power(asset_rise_2year,1/2), 7),2)
+            #data.loc[:, 'roe_year_pb6'] = np.round(np.power(roe_year, 6),2)
+            data.loc[:, 'roe_year_pb7'] = np.round(np.power(roe_year, 7),2)
+            data.loc[:, 'roe_half_year_pb7'] = np.round(np.power(roe_half_year, 7), 2)
+
+            ''' 
+            fcff  float 自由现金流
+            ocfps 	float	每股经营活动产生的现金流量净额
+            cfps	float	每股现金流量净额
+            ebit_ps	float	每股息税前利润
+            ebit 息税前利润
+            bps  每股净资产
+            profit_dedt 扣除非经常性损益后的净利润
+            q_dtprofit 扣除非经常损益后的单季度净利润
+            q_opincome 经营活动单季度净收益
+            q_ocf_to_or 经营活动产生的现金流量净额／经营活动净收益(单季度)
+            '''
+            data.loc[:,'netasset'] = data.ebit/data.ebit_ps*data.bps #净资产
+            data.loc[:,'cash'] = data.ebit/data.ebit_ps*data.cfps #现金流
+            data.loc[:,'q_ocf'] = data.q_opincome*data.q_ocf_to_or #单季度经营活动产生的现金流量
+            data.loc[:,'q4_ocf'] = QA.EMA(data['q_ocf'], 4)
+            data.loc[:, 'q4_opincome'] = QA.EMA(data['q_opincome'], 4)
+            data.loc[:, 'q4_dtprofit'] = QA.EMA(data['q_dtprofit'], 4)
             return data
 
         self.finacial = self.finacial.groupby('ts_code').apply(_indicator)
 
 
         basic = self.basic
-        basic.loc[:,'equity_pb6'] = 0
-        basic.loc[:,'equity_pb7'] = 0
-        basic.loc[:,'roe_year_pb6'] = 0
+        basic.loc[:,'equity2_pb7'] = 0
+        basic.loc[:,'equity3_pb7'] = 0
         basic.loc[:,'roe_year_pb7'] = 0
+        basic.loc[:, 'roe_half_year_pb7'] = 0
         basic.loc[:,'equity_rejust'] = 0
 
         def _indicatorCp(data):
             fin = self.finacial[self.finacial.ts_code==data.name]
             ast = self.asset.loc[self.asset.ts_code==data.name]
-            for i in range(0, len(fin)):
-                #equity_rejust
-                #print(index)
-                #print(index)
-                #print( '@@@@@fin.rows='+str(fin.shape[0]))
+            for i in range(0, fin.shape[0]):
                 if i+1 < fin.shape[0]:
                     query = (data.trade_date >= fin.iloc[i].ann_date) & (data.trade_date < fin.iloc[i + 1].ann_date)
-                    data.loc[query, ['equity_pb6']] = fin.iloc[i].equity_pb6
-                    data.loc[query, ['equity_pb7']] = fin.iloc[i].equity_pb7
-                    data.loc[query, ['roe_year_pb6']] = fin.iloc[i].roe_year_pb6
+                    data.loc[query, ['equity2_pb7']] = fin.iloc[i].equity2_pb7
+                    data.loc[query, ['equity3_pb7']] = fin.iloc[i].equity3_pb7
+                    data.loc[query, ['roe_half_year_pb7']] = fin.iloc[i].roe_half_year_pb7
                     data.loc[query, ['roe_year_pb7']] = fin.iloc[i].roe_year_pb7
-                    data.loc[query, ['equity_rejust']] = data.loc[query].total_mv / data.loc[query].pb * 10000 / ast[ast.ann_date == fin.iloc[i].ann_date].total_hldr_eqy_exc_min_int
+                    data.loc[query, ['roe_half_year_pb7']] = fin.iloc[i].roe_half_year_pb7
+                    data.loc[query, ['equity_rejust']] = np.round(data.loc[query].total_mv / data.loc[query].pb * 10000 /fin.iloc[i].netasset.values ,2)
                 else:
                     query = data.trade_date >= fin.iloc[i].ann_date
-                    data.loc[query, ['equity_pb6']] = fin.iloc[i].equity_pb6
-                    data.loc[query, ['equity_pb7']] = fin.iloc[i].equity_pb7
-                    data.loc[query, ['roe_year_pb6']] = fin.iloc[i].roe_year_pb6
+                    data.loc[query, ['equity2_pb7']] = fin.iloc[i].equity2_pb7
+                    data.loc[query, ['equity3_pb7']] = fin.iloc[i].equity3_pb7
+                    data.loc[query, ['roe_half_year_pb7']] = fin.iloc[i].roe_half_year_pb7
                     data.loc[query, ['roe_year_pb7']] = fin.iloc[i].roe_year_pb7
-                    data.loc[query, ['equity_rejust']] = data.loc[query].total_mv / data.loc[query].pb * 10000 / ast[ast.ann_date == fin.iloc[i].ann_date].total_hldr_eqy_exc_min_int
+                    data.loc[query, ['equity_rejust']] = np.round(data.loc[query].total_mv / data.loc[query].pb * 10000 / fin.iloc[i].netasset.values,2)
             #print(basic.loc[:,['equity_pb6','equity_pb7','roe_year_pb6','roe_year_pb7']].head())
             return data
 
         # 获取每日equity_pb6、roe_year_pb6
         self.basic = basic.groupby('ts_code').apply(_indicatorCp)
+        self.basic.to_pickle(self.basic_temp_name)
 
 
 
         #每日统计指标
         def _dailystat(df):
             rs = []
-            non_finacial_codes = self.stock[(self.stock != '银行') & (self.stock != '证券')].ts_code.values
-            non_finacial = df[~df.ts_code.isin(non_finacial_codes)]
-            st = non_finacial.loc[:,['equity_pb6','equity_pb7','roe_year_pb6','roe_year_pb7']].describe([.05, .15, .25,.5,.75]).T.reset_index(level=0)
+            non_finacial_codes = self.stock[(self.stock.industry != '银行') & (self.stock.industry != '保险')].ts_code.values
+            non_finacial = df[df.ts_code.isin(non_finacial_codes)]
+
+            non_finacial.loc[:,'equity2_pb7_pb'] = np.round(non_finacial.loc[:,'equity2_pb7']/non_finacial.loc[:,'pb'],3)
+            non_finacial.loc[:, 'equity3_pb7_pb'] = np.round(non_finacial.loc[:, 'equity3_pb7'] / non_finacial.loc[:, 'pb'],3)
+            non_finacial.loc[:, 'roe_half_year_pb7_pb'] = np.round(non_finacial.loc[:, 'roe_half_year_pb7'] / non_finacial.loc[:, 'pb'],3)
+            non_finacial.loc[:, 'roe_year_pb7_pb'] = np.round(non_finacial.loc[:, 'roe_year_pb7'] / non_finacial.loc[:, 'pb'],3)
+            # 太假的不要，干扰数据，净资产本季报之后发生变化>1.1的排除
+            non_finacial = non_finacial.loc[(non_finacial.equity2_pb7 < 11) & (non_finacial.equity_rejust < 1.1) & (non_finacial.roe_year_pb7_pb < 11)]
+            st = non_finacial.loc[:,['equity2_pb7_pb','equity3_pb7_pb','roe_half_year_pb7_pb','roe_year_pb7_pb']].describe([.25,.5,.75,.85,.95]).T.reset_index(level=0)
+            st.columns = ['category','cnt','mean','std','min','per25','per50','per75','per85','per95','max']
             st.loc[:,'statype'] ='non_finacial'
             st.index = [df.name]*4
             rs.append(st)
@@ -92,8 +130,7 @@ class simpleValued:
         self.dailymarket = self.basic.groupby('trade_date').apply(_dailystat)
 
 
-
-    def top5_valued(self,df):
+    def non_finacal_top5_valued(self,df):
         """
         简单价值判断，近1年 roe,近半年roe，近3年净资产增速，近2年净资产增速。价值法6年pb、7年pb
         """
@@ -102,12 +139,62 @@ class simpleValued:
         # basic.equity_pb6/basic.pb
         # td = basic.merge(dailymarket[['trade_date', 'equity_pb6top5', 'equity_pb7top5','roe_year_pb6top5','roe_year_pb7top5']], left_on='trade_date', right_on='trade_date', how='left').set_index(basic.index)
         # td
+        non_finacial_codes = self.stock[(self.stock != '银行') & (self.stock.industry != '保险')].ts_code.values
+        basic = self.basic[self.basic.ts_code.isin(non_finacial_codes)]
+
+        def _trash_fiter(df):
+            '''垃圾排除大法 剔除商誉过高、现金流不充裕，主营利润占比低、资产负债率过高、存货占比、应收占比'''
+            fin = self.finacial[self.finacial.ts_code == df.name]
+            ast = self.asset.loc[self.asset.ts_code == df.name]
+            rm = None
+            rms = []
+            fin.loc[:, 'rmflag'] = 0
+            fin.loc[fin.q4_ocf/fin.q4_opincome<0.6] = 1 #经营活动现金流/经营活动净利润 <0.6的不要了
+            fin.loc[fin.q4_opincome / fin.q4_dtprofit < 0.7] = 1  # 经营活动净收益/净利润 <0.7的不要了（投资收益什么的不靠谱）
+            ast.loc[:, 'rmflag'] = 0
+            ast.loc[ast.goodwill / ast.total_hldr_eqy_exc_min_int > 0.2,'rmflag'] = 1  # 商誉占比
+            ast.loc[ast.inventories / ast.total_hldr_eqy_exc_min_int > 0.3,'rmflag'] = 1  # 存货占比
+            ast.loc[(ast.notes_receiv + ast.accounts_receiv) / ast.total_hldr_eqy_exc_min_int > 0.2,'rmflag'] = 1  # 应收占比
+
+            for i in range(0, ast.shape[0]):
+                if ast.iloc[i].rmflag == 1 and not rm:
+                    rm = ast.iloc[i].ann_date
+                if ast.iloc[i].rmflag == 0 and not rm:
+                    rms.append((rm, ast.iloc[i].ann_date))
+                    rm = None
+            if not rm:
+                rms.append((rm, self.end))
+                rm = None
+
+            for i in range(0, fin.shape[0]):
+                if fin.iloc[i].rmflag == 1 and not rm:
+                    rm = fin.iloc[i].ann_date
+                if fin.iloc[i].rmflag == 0 and not rm:
+                    rms.append((rm, fin.iloc[i].ann_date))
+                    rm = None
+            if not rm:
+                rms.append((rm, self.end))
+                rm = None
+
+            data = df
+            for k in rms:
+                data = data[~((data.trade_date >= k[0]) & (data.trade_date < k[1]))]
+                return data
+        basic = basic.groupby('ts_code').apply(_trash_fiter)
+
+
         def _top5(df):
-            dailymarket = self.dailymarket[(self.dailymarket.statype == 'non-finacial') & (self.dailymarket.trade_date == df[0].trade_date)]
-            buy = df.equity_pb6/df.pb - dailymarket.equity_pb6top5
-            sell = df.equity_pb6/df.pb - dailymarket.equity_pb6top15
+            dailymarket = self.dailymarket[(self.dailymarket.statype == 'non-finacial') & (self.dailymarket.trade_date == df.name)]
+            buy = df.equity2_pb7/df.pb - dailymarket[dailymarket.category=='equity2_pb7_pb'].per95
+            sell = dailymarket[dailymarket.category=='equity2_pb7_pb'].per95 - df.equity2_pb7/df.pb - 0.3
             return pd.DataFrame({'buy': buy, 'sell': sell})
-        return self.basic.groupby(level=1, sort=False).apply(_top5).set_index(['trade_date', 'ts_code'])
+
+        return basic.groupby('ts_code').apply(_top5).set_index(['trade_date', 'ts_code'])
+        #return basic.groupby(level=1, sort=False).apply(_top5).set_index(['trade_date', 'ts_code'])
+
+    def price_trend(self,df):
+
+        pass
 
 if __name__ == '__main__':
     #finacial = pro.QA_fetch_get_finindicator(start='20100101', end='20181231',code=['006160.SH','002056.SZ'])
@@ -117,19 +204,6 @@ if __name__ == '__main__':
     #asset = pro.QA_fetch_get_assetAliability(start='20160101', end='20180930')
     #print(asset.head())
 
-    def _indicator(data):
-        #print(data.head())
-        roe_year = QA.EMA(data['q_dt_roe'], 4) * 4
-        roe_half_year = QA.EMA(data['q_dt_roe'], 2) * 4
-        # 近2年净资产收益率
-        asset_rise_2year = data['equity_yoy'].shift(4) * data['equity_yoy']
-        # 近3年净资产收益率
-        asset_rise_3year = data['equity_yoy'].shift(8) * asset_rise_2year
-        equity_pb6 = np.power(asset_rise_3year, 6);
-        equity_pb7 = np.power(asset_rise_3year, 7);
-        roe_year_pb6 = np.power(roe_year, 6);
-        roe_year_pb7 = np.power(roe_year, 7);
-        return pd.DataFrame({'roe_year': roe_year, 'roe_half_year': roe_half_year, 'asset_rise_2year': asset_rise_2year, 'asset_rise_3year': asset_rise_3year, 'equity_pb6': equity_pb6, 'roe_year_pb6': roe_year_pb6})
 
 
     #fin = finacial.groupby('ts_code').apply(_indicator)
--- EXAMPLE/test_backtest/example/simple_valued_season.py
+++ EXAMPLE/test_backtest/example/simple_valued_season.py
@@ -2,7 +2,7 @@
 import math
 import QUANTAXIS as QA
 from QUANTAXIS.QAFetch import QATusharePro as pro
-from indicator.simple_valued import simple_valued
+from indicator.simple_valued import simpleValued
 
 try:
     if QA.__version__>'1.1.0':
@@ -29,24 +29,25 @@ def simple_backtest(AC:QA.QA_Account, code, start:str, end:str):
     # todo 计算信号
     sv = simpleValued(start,end)
     sv.indcators_prepare()
-    stock_signal = sv.top5_valued()
+    stock_signal = sv.non_finacal_top5_valued()
     for item in stock_signal.index.levels[0]:
         peroid_item = stock_signal.xs(item,
                 level=0,
                 drop_level=False)
         # 可卖数量
-        buy_candidates = peroid_item[peroid_item.buy>=0]
-        sell_candidates = peroid_item[peroid_item.sell<=0]
-        if not sell_candidates.empty:
-            sells = sell_candidates.union(AC.hold_table().code.unique())
+        buy_candidates = peroid_item[peroid_item.buy>0]
+        sell_candidates = peroid_item[peroid_item.sell<0]
+        elimits = AC.hold_table().code.diff(peroid_item.ts_code)#基本面发生变化，直接清空
+        if not sell_candidates.empty or not elimits.empty:
+            sells = sell_candidates.ts_code.intersection(AC.hold_table().code).union(elimits)
             for code in sells:
                 cur_account_sotck_code_sell_available_amount = AC.sell_available.get(code, 0)
                 order = AC.send_order(
-                    code=code, time=cur_stock_date, towards=QA.ORDER_DIRECTION.SELL,
-                    order_model=QA.ORDER_MODEL.MARKET, amount_model=QA.AMOUNT_MODEL.BY_AMOUNT,
+                    code=code, time=peroid_item.trade_date[0], towards=QA.ORDER_DIRECTION.SELL,
+                    order_model=QA.ORDER_MODEL.CLOSE, amount_model=QA.AMOUNT_MODEL.BY_AMOUNT,
                     amount=cur_account_sotck_code_sell_available_amount, price=0,
                 )
-                Broker.receive_order(QA.QA_Event(order=order, market_data=stock_item))
+                Broker.receive_order(QA.QA_Event(order=order))
                 trade_mes = Broker.query_orders(AC.account_cookie, 'filled')
                 res = trade_mes.loc[order.account_cookie, order.realorder_id]
                 order.trade(res.trade_id, res.trade_price,
@@ -58,15 +59,73 @@ def simple_backtest(AC:QA.QA_Account, code, start:str, end:str):
                 avgPrice = peroid_item[peroid_item.code==code].close
                 maxMoney = cash/len(buy_candidates)
                 order = AC.send_order(
-                    code=cur_stock_code, time=cur_stock_date, towards=QA.ORDER_DIRECTION.BUY,
+                    code=code, time=peroid_item.trade_date[0], towards=QA.ORDER_DIRECTION.BUY,
                     order_model=QA.ORDER_MODEL.MARKET,
                     amount_model=QA.AMOUNT_MODEL.BY_MONEY,
                     money=maxMoney,
                     price=avgPrice,
                 )
-                Broker.receive_order(QA.QA_Event(order=order, market_data=stock_item))
+                Broker.receive_order(QA.QA_Event(order=order))
                 trade_mes = Broker.query_orders(AC.account_cookie, 'filled')
                 res = trade_mes.loc[order.account_cookie, order.realorder_id]
                 order.trade(res.trade_id, res.trade_price,
                             res.trade_amount, res.trade_time)
         AC.settle()
+
+if __name__ == '__main__':
+    # 策略名称
+    strategy_name = 'MACD_JCSC'
+    # 用户cookie
+    user_cookie = 'user1'
+    # 组合cookie
+    portfolio_cookie = 'win300'
+    # 账户cookie
+    account_cookie = 'bba'
+    benchmark_code = '000300'
+    initial_cash = 200000
+    initial_hold = {}
+    # 交易佣金
+    commission_coeff = 0.00025
+    # 印花税
+    tax_coeff = 0.0015
+
+    # backtest_code_list = QA.QA_fetch_stock_block_adv().code[0:10]
+    backtest_code_list = '000001'
+    backtest_start_date = '2018-01-01'
+    backtest_end_date = '2018-08-21'
+
+    Broker = QA.QA_BacktestBroker()
+    AC = QA.QA_Account(
+        strategy_name=strategy_name,
+        user_cookie=user_cookie,
+        portfolio_cookie=portfolio_cookie,
+        account_cookie=account_cookie,
+        init_hold=initial_hold,
+        init_cash=initial_cash,
+        commission_coeff=commission_coeff,
+        tax_coeff=tax_coeff,
+        market_type=QA.MARKET_TYPE.STOCK_CN,
+        frequence=QA.FREQUENCE.DAY
+    )
+    # 设置初始资金
+    # AC.reset_assets(20000000)
+
+    # 回测
+    simple_backtest(AC, backtest_code_list, backtest_start_date, backtest_end_date)
+
+    AC.save()
+
+    # 结果
+    print(AC.message)
+    print(AC.history_table)
+
+
+
+    # 分析
+    risk = QA.QA_Risk(account=AC, benchmark_code=benchmark_code, benchmark_type=QA.MARKET_TYPE.INDEX_CN, if_fq=False)
+    risk.save()
+
+    print(risk.message)
+    fig=risk.plot_assets_curve()
+
+    fig.plot()
