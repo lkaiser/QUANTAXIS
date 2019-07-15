d3a9f9503a7c6c75147e6946996ddcef719dce38
lkaiser <minijjlk@gmail.com>
2019-07-15 08:37:26 +0800

    simple strategy

--- EXAMPLE/test_backtest/example/indicator/macd.py
+++ EXAMPLE/test_backtest/example/indicator/macd.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import QUANTAXIS as QA
 import pandas as pd
+import functools
 
 def MACD_JCSC(dataframe, SHORT=12, LONG=26, M=9):
     """
@@ -16,3 +17,32 @@ def MACD_JCSC(dataframe, SHORT=12, LONG=26, M=9):
     CROSS_SC = QA.CROSS(DEA, DIFF)
     ZERO = 0
     return pd.DataFrame({'DIFF': DIFF, 'DEA': DEA, 'MACD': MACD, 'CROSS_JC': CROSS_JC, 'CROSS_SC': CROSS_SC, 'ZERO': ZERO})
+
+from functools import wraps
+def wrap3(func):
+    @wraps(func)
+    def call_it(*args, **kwargs):
+        """wrap func: call_it2"""
+        print('before call')
+        return func(*args, **kwargs)
+    return call_it
+@wrap3
+def hello3():
+    """test hello 3"""
+    print('hello world3')
+
+def hello4():
+    print('hello world4')
+
+def delayed_function():
+    if 1:
+        a = 2
+    else:
+        a = 3
+    print(a)
+
+if __name__ == '__main__':
+    print('wtf')
+    hello3()
+    a = functools.wraps(hello4)(delayed_function)
+    a()
